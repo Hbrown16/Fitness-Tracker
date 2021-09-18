615c64363a9fddaa7c36fceac8d9989b52130636
Hbrown16 <hbrown1094@gmail.com>
2021-09-18 10:11:25 -0400

    .post rout done

--- Develop/Routes/api routes.js
+++ Develop/Routes/api routes.js
@@ -33,5 +33,7 @@ router.put("/api/workouts/:id", (req, res) => {
 router.post("/api/workouts", ({body}, res) => {
     db.Workout.create(body).then((dbWorkout => {
         res.json(dbWorkout);
-    }))
-})
+    })).catch(err => {
+        res.json(err);
+    });
+});
