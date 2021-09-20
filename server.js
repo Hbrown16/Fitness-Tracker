9336f629fb8d3bba23e65100a08e73122176dea9
Hbrown16 <hbrown1094@gmail.com>
2021-09-20 15:31:42 -0400

    changed port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 
-const PORT = process.env.Port || 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 require('dotenv').config();
 app.use(logger("dev"));
